5762c645a2fc9966fb9c5900b4f92a6a075697d5
Tommyboy0797 <thomas.barley@btinternet.com>
2025-04-29 08:03:53 +0100

    asd

--- python/asc_dev_img_gen.py
+++ python/asc_dev_img_gen.py
@@ -1,16 +1,18 @@
 from tkinter import Tk, filedialog
-from PIL import imgage
+from PIL import Image
 
 def open_and_overlay():
     basepath = filedialog.askopenfilename(title="open base picture")
-    base = imgage.open(basepath).convert("RGBA")
+    base = Image.open(basepath).convert("RGBA")
 
     overlay1_path = filedialog.askopenfilename(title="open top left overlay")
     overlay2_path = filedialog.askopenfilename(title="open top right overlay")
-    overlay1 = imgage.open(overlay1_path).convert("RGBA")
-    overlay2 = imgage.open(overlay2_path).convert("RGBA")
+    overlay1 = Image.open(overlay1_path).convert("RGBA")
+    overlay2 = Image.open(overlay2_path).convert("RGBA")
 
     base.paste(overlay1, (0,0), overlay1)
     base.paste(overlay2, (base.width - overlay2.width, 0), overlay2)
 
-    base.save(f"converted{basepath}.png")
+    base.save(f"converted.png")
+
+open_and_overlay()
