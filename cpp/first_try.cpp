8dd2b07316855099e5ccd2050488333c488b9aeb
Tommyboy0797 <thomas.barley@btinternet.com>
2025-05-01 22:44:25 +0100

    cpp stuff

---
+++ cpp/first_try.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //  is a header file library that lets us work with input and output objects, such as cout. These header things add stuff
+
+
+int main() {
+    std::cout << "Test Test 123!";
+    return 0;
+}
