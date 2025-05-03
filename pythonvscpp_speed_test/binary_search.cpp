576a35cc146d8d47bb334ce8477331fa9e268edd
Tommyboy0797 <thomas.barley@btinternet.com>
2025-05-04 00:01:32 +0100

    increase speed, cpp now 20x faster

--- pythonvscpp_speed_test/binary_search.cpp
+++ pythonvscpp_speed_test/binary_search.cpp
@@ -29,14 +29,14 @@ int main() {
     // Benchmarking: measuring the execution time of 1000 runs
     auto start = chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 1000000; ++i) {
         binary_search(arr, target);
     }
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> duration = end - start;
 
-    cout << "Execution time for 1000 runs: " << duration.count() << " seconds" << endl;
+    cout << "Execution time for 1 million runs: " << duration.count() << " seconds" << endl;
     cout << "Found the value at index " << mid << endl;
     return 0;
 }
